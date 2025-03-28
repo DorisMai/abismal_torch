920f51094baa3bbe3d80c0b7edb6ec06909743d1
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-03-27 23:21:42 -0700

    make runs longer

--- abismal_torch/command_line/abismal.py
+++ abismal_torch/command_line/abismal.py
@@ -175,7 +175,7 @@ def main():
     trainer = L.Trainer(
         deterministic=True,
         accelerator=args.accelerator,
-        max_epochs=args.epochs,
+        min_steps=args.epochs * args.steps_per_epoch,
         default_root_dir=args.out_dir,
         callbacks=callbacks,
         log_every_n_steps=1,
