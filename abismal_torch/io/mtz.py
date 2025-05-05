fixed ll weighting issue. reduces tensor moving between devices.

abismal_torch/io/mtz.py
@@ -73,8 +73,8 @@ class MTZDataset(Dataset):
         ds["image_id"] = ds.groupby(self.batch_key).ngroup()
         ds.sort_values("image_id", inplace=True)
 
-        self.image_id = torch.tensor(ds.image_id.to_numpy("int32"))
-        self.rasu_id = torch.ones_like(self.image_id, dtype=torch.int32) * self.rasu_id
+        self.image_id = torch.tensor(ds.image_id.to_numpy("int64"))
+        self.rasu_id = torch.ones_like(self.image_id, dtype=torch.int64) * self.rasu_id
         self.hkl_in = torch.tensor(ds.get_hkls())
         self.resolution = torch.tensor(ds.dHKL.to_numpy())
         self.wavelength = (

abismal_torch/layers/average.py
@@ -2,22 +2,26 @@ import torch
 
 
 class ImageAverage(torch.nn.Module):
-    def forward(self, x: torch.Tensor, image_id: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self, x: torch.Tensor, image_id: torch.Tensor) -> torch.Tensor:
         """
-        Average x features by image_id.
+        Average x along images and across mc samples.
 
         Args:
-            x (torch.Tensor): A float tensor of shape (n_refln, n_feature).
-            image_id (torch.Tensor): An int tensor of shape (n_refln).
+            x (torch.Tensor): Source value tensor of shape (n_reflns, n_dim).
+            image_id (torch.Tensor): A tensor of shape (n_reflns,) that contains the
+                image index for each reflection.
 
         Returns:
-            xout (torch.Tensor): A float tensor of shape (n_images, n_feature).
+            averaged (torch.Tensor): A tensor of shape (n_images, n_dim).
+            counts (torch.Tensor): A tensor of shape (n_images,) that contains the
+                number of reflections in each image.
         """
-        n_images = image_id.max() + 1
-        n_features = x.shape[-1]
-        idx = torch.tile(image_id[:, None], (1, n_features)).to(dtype=torch.int64, device=x.device)
-        xout = torch.zeros((n_images, n_features)).type_as(x)
-        xout.scatter_add_(dim=0, index=idx, src=x)
-        n_reflns_per_image = torch.bincount(image_id).to(dtype=torch.int64, device=x.device)
-        xout /= n_reflns_per_image[:, None]
-        return xout
+        unique_image_ids, unique_indices, counts = torch.unique(image_id, return_inverse=True, return_counts=True)
+        n_images = unique_image_ids.size(0)
+        _, n_dim = x.shape
+        _averaged = x.new_zeros((n_images, n_dim))
+        idx = unique_indices[:, None].expand(-1, n_dim)
+        _averaged.scatter_add_(dim=0, index=idx, src=x)
+        _averaged /= counts[:, None]
+        return _averaged, unique_indices, counts

abismal_torch/merging/merging.py
@@ -4,6 +4,7 @@ import torch
 
 from abismal_torch.distributions import compute_kl_divergence
 from abismal_torch.layers import LazyWelfordStandardization as Standardization
+from abismal_torch.layers.average import ImageAverage
 from abismal_torch.prior.base import PriorBase
 from abismal_torch.surrogate_posterior.base import PosteriorBase
 from abismal_torch.symmetry import Op
@@ -46,29 +47,7 @@ class VariationalMergingModel(torch.nn.Module):
         self.reindexing_ops = [Op(op) for op in reindexing_ops]
         self.standardize_intensity = Standardization(center=False)
         self.standardize_metadata = Standardization(center=True)
-
-    def average_by_images(
-        self, source_value: torch.Tensor, image_id: torch.Tensor
-    ) -> torch.Tensor:
-        """
-        Average source_value along images and across mc samples.
-
-        Args:
-            source_value (torch.Tensor): A tensor of shape (n_reflns, n_samples).
-            image_id (torch.Tensor): A tensor of shape (n_reflns,) that contains the
-                image index for each reflection.
-
-        Returns:
-            averaged (torch.Tensor): A tensor of shape (n_images,).
-        """
-        unique_image_ids, unique_indices, counts = torch.unique(image_id, return_inverse=True, return_counts=True)
-        n_images = unique_image_ids.size(0)
-        _, mc_samples = source_value.shape
-        _averaged = torch.zeros((n_images, mc_samples)).type_as(source_value)
-        idx = torch.tile(unique_indices[:, None], (1, mc_samples)).to(dtype=torch.int64, device=source_value.device)
-        _averaged.scatter_add_(dim=0, index=idx, src=source_value)
-        _averaged = _averaged.sum(dim=1) / counts / mc_samples
-        return _averaged
+        self.pool = ImageAverage()
 
     def standardize_inputs(
         self, inputs: dict[str, torch.Tensor]
@@ -142,6 +121,7 @@ class VariationalMergingModel(torch.nn.Module):
         ll = None
         ipred = None
         hkl = None
+        reflns_per_image = None
         for op in self.reindexing_ops:
             _hkl = op(hkl_in)
             _ipred = self.surrogate_posterior.rac.gather(
@@ -150,7 +130,8 @@ class VariationalMergingModel(torch.nn.Module):
             _ipred = torch.square(_ipred) * scale
 
             _ll = self.likelihood(_ipred, iobs, sigiobs)
-            _ll = self.average_by_images(_ll, image_id)  # Shape (n_images,)
+            _ll, _, reflns_per_image = self.pool(_ll, image_id)  # Shape (n_images, mc_samples), _, (n_images,)
+            _ll = _ll.mean(dim=1) # Shape (n_images,)
 
             if ll is None:
                 ipred = _ipred
@@ -165,8 +146,9 @@ class VariationalMergingModel(torch.nn.Module):
                 hkl = torch.where(
                     idx[image_id].unsqueeze(-1), _hkl, hkl
                 )  # Shape (n_refln, 3)
-        # Output predictions and losses
-        ipred_avg = torch.mean(ipred, dim=-1)  # Shape (n_refln,)
+
+        # Reweight likelihood by number of reflections in each image
+        ll = ll * reflns_per_image / reflns_per_image.sum()
         
         # if ipred or ll any is not finite, drop into IPython
         if not torch.all(torch.isfinite(ipred)) or not torch.all(torch.isfinite(ll)):
@@ -174,10 +156,10 @@ class VariationalMergingModel(torch.nn.Module):
             embed(colors='linux')
 
         return {
-            "ipred_avg": ipred_avg,
             "loss_nll": -ll, #shape (n_images,)
             "loss_kl": kl_div, #shape (rac_size,)
             "scale_kl_div": scale_kl_div, #shape (n_reflns,)
+            "ipred_avg": torch.mean(ipred, dim=-1), #shape (n_reflns,)
             "hkl": hkl, #shape (n_reflns, 3)
             "z": z, #shape (mc_samples, rac_size)
             "scale": scale, #shape (mc_samples, n_reflns)

abismal_torch/scaling/scaling.py
@@ -144,14 +144,12 @@ class ImageScaler(nn.Module):
         image = self._create_image(inputs)  # Shape (n_reflns, n_features + 2)
         image_embeddings = self.image_linear_in(image)  # Shape (n_reflns, mlp_width)
         image_embeddings = self.mlp(image_embeddings)  # Shape (n_reflns, mlp_width)
-        image_embeddings = self.pool(
+        image_embeddings, image_idx, _ = self.pool(
             image_embeddings, image_id
         )  # Shape (n_images, mlp_width)
 
         scale_embeddings = self.scale_linear_in(metadata)  # Shape (n_reflns, mlp_width)
-        scale_embeddings = (
-            scale_embeddings + image_embeddings[image_id]
-        )  # Shape (n_reflns, mlp_width)
+        scale_embeddings = scale_embeddings + image_embeddings[image_idx]
         scale_embeddings = self.scale_mlp(
             scale_embeddings
         )  # Shape (n_reflns, mlp_width)