a4e6a41fcd6c3cd362c6c2db4bc9c7ca483d5e15
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-05-17 15:04:46 -0700

    assume isomorphous

--- abismal_torch/command_line/abismal.py
+++ abismal_torch/command_line/abismal.py
@@ -136,9 +136,9 @@ def main():
     L.seed_everything(args.seed)
 
     # ========== load data ==========#
-    from abismal_torch.io.manager import MTZDataModule
+    from abismal_torch.io.manager import IsomorphousDataModule
 
-    data = MTZDataModule(
+    data = IsomorphousDataModule(
         args.inputs,
         batch_size=args.batch_size,
         num_workers=args.num_cpus,
