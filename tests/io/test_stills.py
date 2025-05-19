bd8999786bef73b4c6e451785d2afdbd2b3213ae
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-05-19 09:41:25 -0700

    test more indices

--- tests/io/test_stills.py
+++ tests/io/test_stills.py
@@ -3,8 +3,13 @@ from abismal_torch.io.stills import StillsDataset
 
 
 def test_stills_dataset(refl_file, expt_file):
+    length = 3
     ds = StillsDataset(refl_file, expt_file)
-    assert len(ds) == 3
+    assert len(ds) == length
     for batch in ds:
         pass
 
+    ds[0]
+    ds[length - 1]
+    ds[-length]
+    
